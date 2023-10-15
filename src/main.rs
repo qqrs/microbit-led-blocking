994e7b7b65e4df5082cdd8ef05bf3bbfbfba6253
Kevan Hollbach <khollbach@gmail.com>
2023-10-15 16:54:15 -0400

    Hello interrupts

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
+
+use cortex_m::{asm, interrupt::Mutex, peripheral::NVIC};
 use cortex_m_rt::entry;
-use cortex_m::asm;
 use microbit::{
-    Board,
-    hal::{
-        prelude::*,
-        Timer,
-    }
+    hal::{prelude::*, timer::{Periodic, Instance}, Clocks, Timer},
+    pac::{interrupt, Interrupt, TIMER0},
+    Board, Peripherals,
 };
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
@@ -24,37 +24,42 @@ fn main() -> ! {
     let stat = board.CLOCK.hfclkstat.read().bits();
     rprintln!("{:032b}", stat);
 
-    board.CLOCK.tasks_hfclkstart.write(|w| w.tasks_hfclkstart().trigger());
+    let clocks = Clocks::new(board.CLOCK);
+    clocks.enable_ext_hfosc();
 
-    for _ in 0..(1_600_000/4) {
-        asm::nop()
-    }
-
-    let stat = board.CLOCK.hfclkstat.read().bits();
+    let clock_periph = unsafe { Peripherals::steal() }.CLOCK;
+    let stat = clock_periph.hfclkstat.read().bits();
     rprintln!("{:032b}", stat);
-
-    //let mut timer = Timer::new(board.TIMER1);
-    let mut ticker = Timer::periodic(board.TIMER2);
-    ticker.start(100_000u32);
 
     toggle(&mut board.display_pins.row1);
     toggle(&mut board.display_pins.col1);
-    
+
+    let mut ticker = Timer::periodic(board.TIMER0);
+    ticker.start(100_000u32);
+    ticker.enable_interrupt();
+    unsafe {
+        NVIC::unmask(Interrupt::TIMER0);
+    }
+
     loop {
-        //for _ in 0..(1_600_000/4) {
-            //asm::nop()
-        //}
+        // nb::block!(ticker.wait()).unwrap();
 
-        //timer.start(100_000u32);
-        //nb::block!(timer.wait());
-
-        nb::block!(ticker.wait());
-
-        //rprintln!("toggle");
-        toggle(&mut board.display_pins.col1);
+        // rprintln!("toggle");
+        // toggle(&mut board.display_pins.col1);
     }
 }
 
+#[interrupt]
+fn TIMER0() {
+    rprintln!("timer 0 fired");
+
+    toggle(&mut board.display_pins.col1);
+
+    // clear the event register
+    cortex_m::interrupt::free(|_cs| {
+        unsafe { Peripherals::steal() }.TIMER0.timer_reset_event();
+    });
+}
 
 fn toggle(pin: &mut dyn StatefulOutputPin<Error = Void>) {
     if pin.is_set_high().void_unwrap() {
